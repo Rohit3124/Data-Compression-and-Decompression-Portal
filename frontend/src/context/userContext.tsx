a96d670401b6596a2b3fd2ad57fb5953c0c1aafb
Rohit3124 <rohitkumawat4493@gmail.com>
2025-06-24 01:01:22 +0530

    added privated route

---
+++ frontend/src/context/userContext.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useState, useEffect } from "react";
+import type { ReactNode, Dispatch, SetStateAction } from "react";
+
+interface UserContextType {
+  currentUser: string | null;
+  setCurrentUser: Dispatch<SetStateAction<string | null>>;
+  isLoading: boolean;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+export const currentUserContext = createContext<UserContextType>({
+  currentUser: null,
+  setCurrentUser: () => {},
+  isLoading: true,
+  setIsLoading: () => {},
+});
+
+interface UserContextProps {
+  children: ReactNode;
+}
+
+export const UserContext: React.FC<UserContextProps> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<string | null>(() => {
+    const savedUser = localStorage.getItem("currentUser");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const savedUser = localStorage.getItem("currentUser");
+    if (savedUser) {
+      setCurrentUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      localStorage.setItem("currentUser", JSON.stringify(currentUser));
+    } else {
+      localStorage.removeItem("currentUser");
+    }
+  }, [currentUser]);
+
+  return (
+    <currentUserContext.Provider
+      value={{ currentUser, setCurrentUser, isLoading, setIsLoading }}
+    >
+      {children}
+    </currentUserContext.Provider>
+  );
+};
+
+export default UserContext;
