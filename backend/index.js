798a423823bdcfcf8079d36d87e7f3021092ef1a
Rohit3124 <rohitkumawat4493@gmail.com>
2025-06-25 18:52:10 +0530

    enable CORS for frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -7,11 +8,20 @@ const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
 const fileRoutes = require("./routes/file");
 
+app.use(
+  cors({
+    origin: "https://data-compression-and-decompression-sigma.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/file", fileRoutes);
+
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to MongoDB..."))
