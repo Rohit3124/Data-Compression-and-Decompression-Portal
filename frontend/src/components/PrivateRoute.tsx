7f062d96f355dd9b01436326a7c4e5972ab14723
Rohit3124 <rohitkumawat4493@gmail.com>
2025-06-25 19:47:02 +0530

    fix private route bug

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -5,7 +5,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const PrivateRoute = () => {
   const { currentUser } = useContext(currentUserContext);
 
-  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+  return currentUser ? <Outlet /> : <Navigate to="/sign-up" />;
 };
 
 export default PrivateRoute;
